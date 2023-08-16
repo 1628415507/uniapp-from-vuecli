68c367b27b7429fd5eb0b4b8964e8ada979f8b9a
hongzf <1628415507@qq.com>
2023-08-16 15:05:56 +0800

    style：首页背景色调整

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,10 +1,10 @@
 <!--
  * @Description: 首页
  * @Date: 2023-08-04 09:27:20
- * @LastEditTime: 2023-08-07 17:03:41
+ * @LastEditTime: 2023-08-16 15:00:47
 -->
 <template>
-	<view>
+	<view class="home-page">
 		<u-swiper :list="swiperList"></u-swiper>
 		<u-search placeholder="请输入" v-model="keyword"></u-search>
 		<!-- <u-icon name="photo" color="#2979ff" size="28"></u-icon> -->
@@ -35,7 +35,11 @@
 	}
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+	page {
+		background: linear-gradient(to bottom, #e8f3f5, #e5f0f5) !important;
+	}
+
 	::v-deep .tabbar-wrap {
 		.u-tabbar__content {
 			box-shadow: 0rpx 0rpx 20rpx 0rpx rgba(0, 0, 0, 0.15) !important;
